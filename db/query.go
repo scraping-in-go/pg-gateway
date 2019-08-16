a1cb0dc5220f5916f19807aa960833f945a672cf
Just1689 <Just1689@gmail.com>
2019-08-16 23:03:26 +0200

    refactor

--- db/query.go
+++ db/query.go
@@ -2,9 +2,7 @@ package db
 
 import (
 	"fmt"
-	"github.com/scraping-in-go/svc-db-gateway/model"
 	"github.com/sirupsen/logrus"
-	"strconv"
 )
 
 func GetEntityAll(entity string) (result chan string, err error) {
@@ -22,7 +20,6 @@ func GetEntityAll(entity string) (result chan string, err error) {
 		conn.Close()
 		return
 	}
-
 	go func() {
 		defer conn.Close()
 		defer close(result)
@@ -40,7 +37,6 @@ func GetEntityAll(entity string) (result chan string, err error) {
 		}
 	}()
 	return
-
 }
 
 func GetEntityMany(entity, field, id string) (result chan string, err error) {
@@ -59,7 +55,6 @@ func GetEntityMany(entity, field, id string) (result chan string, err error) {
 		conn.Close()
 		return
 	}
-
 	go func() {
 		defer conn.Close()
 		defer close(result)
@@ -77,7 +72,6 @@ func GetEntityMany(entity, field, id string) (result chan string, err error) {
 		}
 	}()
 	return
-
 }
 
 func GetEntityByID(entity, id string) (row string, err error) {
@@ -89,49 +83,6 @@ func GetEntityByID(entity, id string) (row string, err error) {
 	}
 	sql := fmt.Sprintf("select row_to_json(%s)as row from %s where id=$1", entity, entity)
 	err = conn.QueryRow(sql, id).Scan(&row)
-	if err != nil {
-		logrus.Errorln(err)
-		return
-	}
-	return
-
-}
-
-func Insert(entity string, insertable model.Insertable) (err error) {
-	conn, err := Connect()
-	defer conn.Close()
-	if err != nil {
-		logrus.Errorln(err)
-		return
-	}
-
-	cols := ""
-	binds := ""
-	colID := 0
-	vals := make([]interface{}, 0)
-	for colName, val := range insertable {
-		va := string(val)
-		if va[:1] == "\"" {
-			va = va[1 : len(va)-1]
-			vals = append(vals, va)
-		} else {
-			vals = append(vals, val)
-		}
-		colID++
-		if cols == "" {
-			cols = colName
-		} else {
-			cols += ", " + colName
-		}
-		if binds == "" {
-			binds = "$" + strconv.Itoa(colID)
-		} else {
-			binds += ", $" + strconv.Itoa(colID)
-		}
-	}
-
-	sql := fmt.Sprintf("insert into %s (%s) values(%s)", entity, cols, binds)
-	_, err = conn.Exec(sql, vals...)
 	if err != nil {
 		logrus.Errorln(err)
 		return
---
+++ db/statement.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"fmt"
+	"github.com/scraping-in-go/svc-db-gateway/model"
+	"github.com/sirupsen/logrus"
+	"strconv"
+)
+
+func Insert(entity string, insertable model.Insertable) (err error) {
+	conn, err := Connect()
+	defer conn.Close()
+	if err != nil {
+		logrus.Errorln(err)
+		return
+	}
+
+	cols := ""
+	binds := ""
+	colID := 0
+	vals := make([]interface{}, 0)
+	for colName, val := range insertable {
+		va := string(val)
+		if va[:1] == "\"" {
+			va = va[1 : len(va)-1]
+			vals = append(vals, va)
+		} else {
+			vals = append(vals, val)
+		}
+		colID++
+		if cols == "" {
+			cols = colName
+		} else {
+			cols += ", " + colName
+		}
+		if binds == "" {
+			binds = "$" + strconv.Itoa(colID)
+		} else {
+			binds += ", $" + strconv.Itoa(colID)
+		}
+	}
+
+	sql := fmt.Sprintf("insert into %s (%s) values(%s)", entity, cols, binds)
+	_, err = conn.Exec(sql, vals...)
+	if err != nil {
+		logrus.Errorln(err)
+		return
+	}
+	return
+
+}
