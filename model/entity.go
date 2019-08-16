ca07d4c77ed2cefb0ef6661cccb8034d65b17c08
Just1689 <Just1689@gmail.com>
2019-08-16 23:27:16 +0200

    clean up

--- model/entity.go
+++ model/entity.go
@@ -2,10 +2,4 @@ package model
 
 import "encoding/json"
 
-type Entity struct {
-	Entity string          `json:"entity"`
-	ID     string          `json:"id"`
-	V      json.RawMessage `json:"v"`
-}
-
 type Insertable map[string]json.RawMessage
