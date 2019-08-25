90f2e3bde530776fa6fe0258f954603b7ff3c835
Just1689 <Just1689@gmail.com>
2019-08-25 15:55:02 +0200

    Get many by field=value

--- client/query_async.go
+++ client/query_async.go
@@ -3,6 +3,7 @@ package client
 import (
 	"github.com/just1689/json2channel/j2c"
 	"github.com/sirupsen/logrus"
+	"io"
 	"net/http"
 )
 
@@ -16,16 +17,34 @@ func GetEntityAllAsync(svr string, entity string) (results chan []byte, err erro
 	}
 
 	results = make(chan []byte)
-	go func() {
-		defer resp.Body.Close()
-		ri := j2c.NewReaderItemFromReadCloser(resp.Body)
-		in := j2c.BuildInterpreter(ri)
-		out := j2c.ReadObjects(in, ".")
-		for o := range out {
-			results <- []byte(o)
-		}
-		close(results)
-	}()
+	go ioReadCloserToByteChanByJsonElement(resp.Body, results)
 	return
+
+}
+
+func GetEntityManyAsync(svr string, entity, field, id string) (results chan []byte, err error) {
+	url := svr + "/" + entity + "/" + field + "/" + id
+	resp, err := http.Get(url)
+	if err != nil {
+		logrus.Error(err)
+		resp.Body.Close()
+		return
+	}
+
+	results = make(chan []byte)
+	go ioReadCloserToByteChanByJsonElement(resp.Body, results)
+	return
+
+}
+
+func ioReadCloserToByteChanByJsonElement(body io.ReadCloser, results chan []byte) {
+	defer body.Close()
+	ri := j2c.NewReaderItemFromReadCloser(body)
+	in := j2c.BuildInterpreter(ri)
+	out := j2c.ReadObjects(in, ".")
+	for o := range out {
+		results <- []byte(o)
+	}
+	close(results)
 
 }
