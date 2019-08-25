2662227dfb364da4ef83a9d53c0b68e04438f4c5
Just1689 <Just1689@gmail.com>
2019-08-25 16:07:21 +0200

    single comparison as query

--- query/splitter.go
+++ query/splitter.go
@@ -1,35 +1,51 @@
 package query
 
-import "strings"
+import (
+	"strings"
+)
 
 type Query struct {
-	Entity string
+	Entity      string
+	Comparisons []Comparison
+}
 
+type Comparison struct {
 	Field      string
 	Comparator string
 	Value      string
 }
 
 func BuildQueryFromURL(u string) (result Query) {
-	result = Query{}
+	result = Query{
+		Comparisons: []Comparison{},
+	}
 	u = u[1:]
 	idx := strings.Index(u, "?")
 	if idx == -1 {
 		return
 	}
-	result.Entity = u[0:idx]
+
+	result.setEntityFromUrl(u, idx)
+
+	//Remove the query from the url
 	u = u[idx+1:]
 
+	//Build a comparison
+	co := Comparison{}
 	idx = strings.Index(u, "=")
-	result.Field = u[0:idx]
+	co.Field = u[0:idx]
 	u = u[idx+1:]
-
 	idx = strings.Index(u, ".")
-	result.Comparator = u[0:idx]
+	co.Comparator = u[0:idx]
 	u = u[idx+1:]
+	co.Value = u[0:idx]
 
-	result.Value = u[0:idx]
+	result.Comparisons = append(result.Comparisons, co)
 
 	return
 
+}
+
+func (q *Query) setEntityFromUrl(url string, idOfQ int) {
+	q.Entity = url[0:idOfQ]
 }
--- query/splitter_test.go
+++ query/splitter_test.go
@@ -9,17 +9,57 @@ func TestBuildQueryFromURLForSimpleQuery(t *testing.T) {
 		t.Log("Got", query.Entity, "and not", "people")
 		t.Fail()
 	}
-	if query.Field != "age" {
-		t.Log("Got", query.Field, "and not", "age")
-		t.Fail()
+
+	if len(query.Comparisons) != 1 {
+		t.Error("Expected one query not ", len(query.Comparisons))
+		return
 	}
-	if query.Comparator != "eq" {
-		t.Log("Got", query.Comparator, "and not", "eq")
-		t.Fail()
+	if query.Comparisons[0].Field != "age" {
+		t.Error("Expected age not ", query.Comparisons[0].Field)
+		return
 	}
-	if query.Value != "25" {
-		t.Log("Got", query.Value, "and not", "25")
-		t.Fail()
+	if query.Comparisons[0].Comparator != "eq" {
+		t.Error("Expected eq not ", query.Comparisons[0].Comparator)
+		return
+	}
+	if query.Comparisons[0].Value != "25" {
+		t.Error("Expected 25 not ", query.Comparisons[0].Value)
+		return
 	}
 
+	//if query.Field != "age" {
+	//	t.Log("Got", query.Field, "and not", "age")
+	//	t.Fail()
+	//}
+	//if query.Comparator != "eq" {
+	//	t.Log("Got", query.Comparator, "and not", "eq")
+	//	t.Fail()
+	//}
+	//if query.Value != "25" {
+	//	t.Log("Got", query.Value, "and not", "25")
+	//	t.Fail()
+	//}
+
 }
+
+//func TestBuildQueryFromURLForMultiQuery(t *testing.T) {
+//	url := "people?age=gte.25&active=is.true"
+//	query := BuildQueryFromURL(url)
+//	if query.Entity != "people" {
+//		t.Log("Got", query.Entity, "and not", "people")
+//		t.Fail()
+//	}
+//	if query.Field != "age" {
+//		t.Log("Got", query.Field, "and not", "age")
+//		t.Fail()
+//	}
+//	if query.Comparator != "eq" {
+//		t.Log("Got", query.Comparator, "and not", "eq")
+//		t.Fail()
+//	}
+//	if query.Value != "25" {
+//		t.Log("Got", query.Value, "and not", "25")
+//		t.Fail()
+//	}
+//
+//}
